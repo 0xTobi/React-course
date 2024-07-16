0c7aa0f3e99cc610564cd0317961fc92453d36f7
Toby <0xdegn@gmail.com>
2024-07-16 18:07:19 +0100

    Added data.js

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,32 +7,32 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-const CORE_CONCEPTS = [
-  {
-    image: componentsImg,
-    title: "Components",
-    description:
-      "The core UI building block - compose the user interface by combining multiple components.",
-  },
-  {
-    image: jsxImg,
-    title: "JSX",
-    description:
-      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
-  },
-  {
-    image: propsImg,
-    title: "Props",
-    description:
-      "Make components configurable (and therefore reusable) by passing input data to them.",
-  },
-  {
-    image: stateImg,
-    title: "State",
-    description:
-      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
-  },
-];
+// const CORE_CONCEPTS = [
+//   {
+//     image: componentsImg,
+//     title: "Components",
+//     description:
+//       "The core UI building block - compose the user interface by combining multiple components.",
+//   },
+//   {
+//     image: jsxImg,
+//     title: "JSX",
+//     description:
+//       "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+//   },
+//   {
+//     image: propsImg,
+//     title: "Props",
+//     description:
+//       "Make components configurable (and therefore reusable) by passing input data to them.",
+//   },
+//   {
+//     image: stateImg,
+//     title: "State",
+//     description:
+//       "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+//   },
+// ];
 
 function CoreConcept(props) {
   return (
