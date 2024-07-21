b5165e1cc6075bb3793fb387aeabf0ae71045f6c
Toby <0xdegn@gmail.com>
2024-07-21 15:05:02 +0100

    _

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,12 +7,22 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function CoreConcept({image, title, description}) {
+// function CoreConcept({ concept }) {
+//   return (
+//     <li>
+//       <img src={concept.image} alt={concept.title} />
+//       <h3>{concept.title}</h3>
+//       <p>{concept.description}</p>
+//     </li>
+//   );
+// }
+
+function CoreConcept(props) {
   return (
     <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
   );
 }
@@ -43,14 +53,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
+            {/* <CoreConcept concept={CORE_CONCEPTS[0]} /> */}
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
